9c374315fdf18cc4befa09b897aa68cf48f56256
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-04-22 11:20:43 +0200

    commit

--- Backend/src/routes/api/index.js
+++ Backend/src/routes/api/index.js
@@ -3,8 +3,14 @@ const axios = require("axios");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const ApiRouter = Router();
 
+// convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
 // "/currentday" takes city name as input data and returns the single day current weather and predication
 // returns the latitude and longitude based on city name
+// api returns temperature in units=metric(degree celsius)
 
 ApiRouter.get("/currentday", async (req, res) => {
   const cityName = req.query.cityName;
@@ -26,9 +32,11 @@ ApiRouter.get("/currentday", async (req, res) => {
 
   try {
     const response = await axios.get(apiQuery);
-    //extracting the relevant information from response
 
-    const { city, coord, sunrise, sunset } = response.data;
+    //extracting the relevant information from response
+    const latitudeCoordinateResponse = response.data.city.coord.lat;
+    const longitudeCoordinateResponse = response.data.city.coord.lon;
+    const cityNameResponse = response.data.city.name;
     const sunriseResponse = response.data.city.sunrise;
     const sunsetResponse = response.data.city.sunset;
 
@@ -41,63 +49,167 @@ ApiRouter.get("/currentday", async (req, res) => {
     const forecastsForDay = response.data.list.map((item) => ({
       date: item.dt_txt.split(" ")[0],
       time: item.dt_txt.split(" ")[1],
-      temperature: item.main.temp,
-      min_temperature: item.main.temp_min,
-      max_temperature: item.main.temp_max,
+      temperature_celsius: item.main.temp,
+      min_temperature_celsius: item.main.temp_min,
+      max_temperature_celsius: item.main.temp_max,
+      temperature_fahrenheit: celsiusToFahrenheit(item.main.temp),
+      min_temperature_fahrenheit: celsiusToFahrenheit(item.main.temp_min),
+      max_temperature_fahrenheit: celsiusToFahrenheit(item.main.temp_max),
       main_description: item.weather[0].main,
       description: item.weather[0].description,
-      windSpeed: item.wind.speed,
+      wind_speed: item.wind.speed,
       humidity: item.main.humidity,
     }));
 
-    console.log("Location:", city);
-    console.log("Sunrise:", sunriseConvert);
-    console.log("Sunset:", sunsetConvert);
+    const responseData = {
+      cityNameResponse,
+      latitudeCoordinateResponse,
+      longitudeCoordinateResponse,
+      sunrise: sunriseConvert,
+      sunset: sunsetConvert,
+      forecasts: forecastsForDay.map((forecast) => ({
+        date: forecast.date,
+        time: forecast.time,
+        temperature_celsius: Math.round(forecast.temperature_celsius),
+        min_temperature_celsius: Math.round(forecast.min_temperature_celsius),
+        max_temperature_celsius: Math.round(forecast.max_temperature_celsius),
+        temperature_fahrenheit: Math.round(forecast.temperature_fahrenheit),
+        min_temperature_fahrenheit: Math.round(
+          forecast.min_temperature_fahrenheit
+        ),
+        max_temperature_fahrenheit: Math.round(
+          forecast.max_temperature_fahrenheit
+        ),
+        main_description: forecast.main_description,
+        description: forecast.description,
+        wind_speed: forecast.wind_speed,
+        humidity: forecast.humidity,
+      })),
+    };
 
-    forecastsForDay.forEach((forecast, index) => {
-      console.log("TODAY'S FORECAST", index + 1);
-      console.log("Today's date:", forecast.date);
-      console.log("Today's time:", forecast.time);
-      console.log("Today's temperature:", forecast.temperature);
-      console.log("Today's minimum temperature:", forecast.min_temperature);
-      console.log("Today's maximum temperature:", forecast.max_temperature);
-      console.log("Today's main_description:", forecast.main_description);
-      console.log("Today's description:", forecast.description);
-      console.log("Today's wind Speed:", forecast.windSpeed);
-      console.log("Today's humidity:", forecast.humidity);
-    });
-    res
-      .status(StatusCodes.OK)
-      .json({ city, coord, sunrise, sunset, forecast: response.data });
+    res.status(StatusCodes.OK).json({ responseData });
   } catch (e) {
     console.log("error occured during fetching weather", e);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Error fetching data");
   }
 });
 
+//interpret the weather condition from the WMO weather code
+function interpretWeatherCode(weatherCode) {
+  switch (weatherCode) {
+    case 0:
+      return "Clear sky";
+    case 1:
+      return "Mainly clear";
+    case 2:
+      return "Partly cloudy";
+    case 3:
+      return "Overcast";
+    case 45:
+      return "Fog";
+    case 48:
+      return "Depositing rime fog";
+    case 51:
+      return "Drizzle: Light";
+    case 53:
+      return "Drizzle: Moderate";
+    case 55:
+      return "Drizzle: Dense intensity";
+    case 56:
+      return "Freezing Drizzle: Light";
+    case 57:
+      return "Freezing Drizzle: Dense intensity";
+    case 61:
+      return "Rain: Slight";
+    case 63:
+      return "Rain: Moderate";
+    case 65:
+      return "Rain: Heavy intensity";
+    case 66:
+      return "Freezing Rain: Light";
+    case 67:
+      return "Freezing Rain: Heavy intensity";
+    case 71:
+      return "Snow fall: Slight";
+    case 73:
+      return "Snow fall: Moderate";
+    case 75:
+      return "Snow fall:Heavy intensity";
+    case 77:
+      return "Snow grains";
+    case 80:
+      return "Rain showers: Slight";
+    case 81:
+      return "Rain showers: Moderate";
+    case 82:
+      return "Rain showers: Violent";
+    case 85:
+      return "Snow showers slight";
+    case 86:
+      return "Snow showers heavy";
+    case 95:
+      return "Thunderstorm: Slight or moderate";
+    case 96:
+      return "Thunderstorm with slight hail";
+    case 99:
+      return "Thunderstorm with heavy hail";
+    default:
+      return "Unknown";
+  }
+}
+
 //  "/upcomingdays" takes the lat and lon from "/currentday"
-//  using the retrieved info., "/upcomingdays" retrieves the weather for the next 7 days
+//  using the retrieved lat and lon, "/upcomingdays" retrieves the weather for the next 7 days
 ApiRouter.get("/upcomingdays", async (req, res) => {
   const { latitude, longitude } = req.query;
 
-  const apiQuery = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min`;
+  const apiQuery = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,wind_speed_10m_max`;
 
   try {
     const response = await axios.get(apiQuery);
 
     const dailyData = response.data.daily;
+    const dates = dailyData.time;
+    const weatherCodes = dailyData.weather_code;
+    const maxTemperaturesCelsius = dailyData.temperature_2m_max;
+    const minTemperaturesCelsius = dailyData.temperature_2m_min;
+    const windSpeeds = dailyData.wind_speed_10m_max;
+    const sunrises = dailyData.sunrise;
+    const sunsets = dailyData.sunset;
 
-    dailyData.time.forEach((date, index) => {
-      const maxTemperature = dailyData.temperature_2m_max[index];
-      const minTemperature = dailyData.temperature_2m_min[index];
+    const responseData = {
+      forecasts: dates.map((date, index) => {
+        const maxTemperatureCelsius = maxTemperaturesCelsius[index];
+        const minTemperatureCelsius = minTemperaturesCelsius[index];
+        const windSpeed = windSpeeds[index];
+        const sunrise = sunrises[index].substring(11, 16);
+        const sunset = sunsets[index].substring(11, 16);
+        const weatherCode = weatherCodes[index];
+        const weatherCondition = interpretWeatherCode(weatherCode);
 
-      console.log("UPCOMING FORECAST", index + 1);
-      console.log("Date:", date);
-      console.log("Minimum Temperature:", minTemperature + "°C");
-      console.log("Maximum Temperature:", maxTemperature + "°C");
-    });
+        return {
+          date,
+          temperature_celsius: Math.round(maxTemperatureCelsius),
+          min_temperature_celsius: Math.round(minTemperatureCelsius),
+          max_temperature_celsius: Math.round(maxTemperatureCelsius),
+          temperature_fahrenheit: Math.round(
+            celsiusToFahrenheit(maxTemperatureCelsius)
+          ),
+          min_temperature_fahrenheit: Math.round(
+            celsiusToFahrenheit(minTemperatureCelsius)
+          ),
+          max_temperature_fahrenheit: Math.round(
+            celsiusToFahrenheit(maxTemperatureCelsius)
+          ),
+          wind_speed: windSpeed,
+          sunrise,
+          sunset,
+          description: weatherCondition,
+        };
+      }),
+    };
 
-    res.status(StatusCodes.OK).json(response.data);
+    res.status(StatusCodes.OK).json({ responseData });
   } catch (e) {
     console.log("error occurred during fetching upcoming days weather", e);
     res
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,34 @@
-// -------------- STYLE -----------------
-import CurrentCity from "../components/CurrentCity/CurrentCity";
-import QuickStats from "../components/QuickStats/QuickStats";
+// tools
+import { useState } from "react";
+
+// style
 import style from "./Home.module.css";
-import bg from "../assets/img/bg.jpg";
-import InputFull from "../components/InputFull/InputFull.js";
-import Forecast7Days from "../components/Forecast7Days/Forecast7Days.js";
-import Forecast7DaysComponent from "../components/Forecast7Days/Forecast7DaysComponent.js";
 
+// image
+import bgImage from "../assets/img/bg.jpg";
+
+// components
+import InputFields from "../components/InputFields/InputFields.js";
+import CurrentCity from "../components/CurrentCity/CurrentCity";
+import ForecastDay from "../components/ForecastDay/ForecastDay.jsx";
 import FlippingCard from "../components/FlippingCard/FlippingCard.js";
-// -------------- LOGIC -----------------
-
-// -------------- ELEMENTS -----------------
-
+import ForecastWeek from "../components/ForecastWeek/ForecastWeek.js";
 function Home() {
+  // more efficent to update the background-img
+  // to update just the img in the component and not the whole component
+  const [bg] = useState(bgImage);
+
   return (
-    <div className="hello mt-10 flex flex-col justify-center items-center">
-      <img src={bg} className={style.bg} alt="background" />
-      <InputFull />
-      <div className="mt-7">
-        <CurrentCity />
+    <div
+      className="flex flex-col items-center w-[100vw] h-[100vh] p-[50px]"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div>
+        <InputFields className="mt-[50px]" />
       </div>
-      <div className="mt-20">
-        <Forecast7DaysComponent />
-        {/* <Forecast7Days /> */}
+      <div className="mt-[50px]">
+        <ForecastDay />
+        <ForecastWeek />
       </div>
     </div>
   );
