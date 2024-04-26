2ba26a3af9835855a8d599ea107c3ec6127fdb8a
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-04-26 10:23:54 +0200

    Favorite updated

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const { WeatherRouter } = require("./weather");
 const { UserRouter } = require("./user");
 const { ApiRouter } = require("./api");
+const { FavRouter } = require("./favorite");
 
 const AppRouter = Router();
 
 AppRouter.use("/weather", WeatherRouter);
 AppRouter.use("/user", UserRouter);
 AppRouter.use("/api/getweather", ApiRouter);
+AppRouter.use("/favorite", FavRouter);
 
 module.exports = { AppRouter };
